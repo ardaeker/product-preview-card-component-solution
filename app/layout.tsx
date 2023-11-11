36f84e9095886f246059543c8bbc6d8501f149f7
Arda Eker <ardaekertr@gmail.com>
2023-11-11 15:19:06 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,21 +6,23 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Arda Eker - Product Preview Card Component Solution",
   description:
-    "Hello, I'm Arda Eker. This website showcases my solution to the <***> challenge on Frontend Mentor.",
+    "Hello, I'm Arda Eker. This website showcases my solution to the Product Preview Card Component challenge on Frontend Mentor.",
   authors: [{ name: "Arda Eker", url: "https://www.ardaeker.com" }],
-  // metadataBase: new URL("<***>"),
-  // openGraph: {
-  //   type: "website",
-  //   url: "/",
-  //   title: "Arda Eker - Product Preview Card Component Solution",
-  //   description:
-  //     "Hello, I'm Arda Eker. This website showcases my solution to the Product Preview Card Component Challenge on Frontend Mentor.",
-  //   images: [
-  //     {
-  //       url: "/preview.png",
-  //     },
-  //   ],
-  // },
+  metadataBase: new URL(
+    "https://ardaeker-product-preview-card-component-solution.vercel.app",
+  ),
+  openGraph: {
+    type: "website",
+    url: "/",
+    title: "Arda Eker - Product Preview Card Component Solution",
+    description:
+      "Hello, I'm Arda Eker. This website showcases my solution to the Product Preview Card Component Challenge on Frontend Mentor.",
+    images: [
+      {
+        url: "/desktop-preview.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
